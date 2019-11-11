1885cfa40cc77a03e895c533f942a6cfd41e887d
zak <zak@zakmiller.com>
2019-11-11 15:15:13 -0500

    Serve static content

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule} from '@nestjs/serve-static'; // New
+import { join } from 'path'; // New
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({ // New
+      rootPath: join(__dirname, '..', 'client/dist'), // New
+    }), // New
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
